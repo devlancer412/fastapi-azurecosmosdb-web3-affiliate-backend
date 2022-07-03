0b378815f45d4ca2f10427e5faae78552b2fb7c3
devlancer412 <devanderson0412@gmail.com>
2022-07-03 07:22:16 -0500

    set config

--- config.py
+++ config.py
@@ -5,14 +5,18 @@ from internal import ConfigBase, UNSET
 
 
 class Configuration(ConfigBase):
-    RPC_URL: str = UNSET
-    AFFILIATE_CALL_ID: str = UNSET
-    PRIVATE_KEY: str = UNSET
+    RPC_URL: str = "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
+    AFFILIATE_CALL_ID: str = (
+        "0xcf856f9756bfa0a667a04a632e2cdbfb2fb9bdd8a09f56c3355f4e6ebdd9ca63"
+    )
+    PRIVATE_KEY: str = (
+        "6f8749b0d24524c8ee094ddac371c8e3ae8771e42505927196b0c8d6b67dd175"
+    )
 
     # Database
-    ACCOUNT_HOST: str = UNSET
-    ACCOUNT_KEY: str = UNSET
-    COSMOS_DATABASE: str = UNSET
+    ACCOUNT_HOST: str = "https://affiliate-database.documents.azure.com:443/"
+    ACCOUNT_KEY: str = "PdeeJoOY7rTwz7fCoFb91CAZFl9Fy0s2DwN8HbPvRbHAOnzj0p9aJfzFhj691jTc3GdUSGeu6zRfssXFaH3lSQ=="
+    COSMOS_DATABASE: str = "AffiliateMarketing"
 
 
 # --- Do not edit anything below this line, or do it, I'm not your mom ----

1cd4c4004a699a2a9c4f884e0c6d83d625da7606
devlancer412 <devanderson0412@gmail.com>
2022-07-03 09:01:00 -0500

    contract changed

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from internal import ConfigBase, UNSET
 class Configuration(ConfigBase):
     RPC_URL: str = "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
     AFFILIATE_CALL_ID: str = (
-        "0xcf856f9756bfa0a667a04a632e2cdbfb2fb9bdd8a09f56c3355f4e6ebdd9ca63"
+        "0xc2cb7bbba2dd4927d34fa647b1538ff08e5a25ec077e22b833690cf0720176bc"
     )
     PRIVATE_KEY: str = (
         "6f8749b0d24524c8ee094ddac371c8e3ae8771e42505927196b0c8d6b67dd175"
